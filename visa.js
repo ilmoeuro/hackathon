1b869ca966ab30ea23c6f9e766b86be757a0b118
ilmoeuro <ilmo.euro@gmail.com>
2013-11-04 16:54:09 +0200

    ** Kuntapeliä

--- visa.js
+++ visa.js
@@ -1,10 +1,48 @@
-var currQuestion, currAnswer;
+(function(){
+var questions = [
+	{text: "Missä sijaitsee seuraava postinumero?",
+	 detail: "50190",
+	 answer: "mikkeli"},
+ 	{text: "Missä sijaitsee seuraava postinumero?",
+	 detail: "50120",
+	 answer: "mikkeli"}
+];
+
+function cap(str) {
+	return str.substring(0, 1).toUpperCase() + str.substring(1);
+}
+
+var currQuestion, pisteet;
+currQuestion = questions[0];
 
 $(function(){
-	$("button").click(function(){
-		currAnswer = $(this).attr("id");
+	pisteet = 0;
+
+
+	function showQuestion() {
+		var questionIndex = (Math.random() * questions.length) | 0;
+		currQuestion = questions[questionIndex];
+		$("#kysymys").html(currQuestion.text);
+		$("#tiedot").html(currQuestion.detail);
+		$("#pistemaara").html(pisteet + "");
+	}
+
+	function processAnswer(answer) {
+		if (answer == currQuestion.answer) {
+			$("#edellinen-vastaus").html("Vastaus " + cap(answer) + " oli oikein!");
+			pisteet++;
+		} else {
+			$("#edellinen-vastaus").html("Vastaus " + cap(answer) + " oli väärin!");
+		}
+	}
+
+	$(".vastausnappi").click(function(){
+		var answer = $(this).attr("id").split("-")[1];
+		var answerResult = processAnswer(answer);
+		showQuestion();
 	});
 
-
+	showQuestion();
 
 });
+}());
