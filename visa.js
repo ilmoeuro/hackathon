75749f5dc3e577e7ec26b77875233329fe44e606
ilmoeuro <ilmo.euro@gmail.com>
2013-11-04 18:40:53 +0200

    ** Uutiset

--- visa.js
+++ visa.js
@@ -6,7 +6,9 @@ function cap(str) {
 	return str.substring(0, 1).toUpperCase() + str.substring(1);
 }
 
-var currQuestion, pisteet;
+var currQuestion, pisteet; // much var
+
+var laskuri = 0;
 
 $(function(){
 	pisteet = 0;
@@ -28,14 +30,42 @@ $(function(){
 				});
 			}
 		}
+		
+		//var 
+		
+		$.ajax({
+			url: "uutiset.xml",
+			dataType: "xml",
+			success: function(data){
+				$(data).find("entry").each(function() {
+					var entry = $(this);
+					$(this).children("category").each(function() {
+						if ($(this).attr("term") == "etelä-savo") {
+							questions.push({
+								text: "Missä kunnassa seuraava uutinen tapahtui?",
+								detail: entry.children('title').text(),
+								answer: entry.children('kunta').text()
+							});
+						};
+					});
+				});
+			}
+		});
 	}
 
 	function showQuestion() {
+		if (laskuri++ >= 20) {
+			alert("Peli loppui! Pisteesi: " + pisteet);
+			pisteet = 0;
+			$("#edellinen-vastaus").html("");
+			laskuri = 1;
+		}
 		var questionIndex = (Math.random() * questions.length) | 0;
 		currQuestion = questions[questionIndex];
 		$("#kysymys").html(currQuestion.text);
 		$("#tiedot").html(currQuestion.detail);
 		$("#pistemaara").html(pisteet + "");
+		$("#laskuri").html(laskuri + "");
 	}
 
 	function processAnswer(answer) {
